e89fa3da0bf9e4d4c6140b874d8e3c9b459c94c1
iambabs <iamadeniyiismail@gmail.com>
2022-04-17 23:40:18 +0200

    Added Simple responsive email template
    TODO:   change font weight of variables

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const handlebars = require("handlebars");
+const cors = require("cors");
 const http = require("http");
 const fs = require("fs");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const server = http.createServer(app);
 
+app.use(cors());
 app.use(express.json());
 app.engine("html", require("hogan-express"));
 app.set("views", __dirname + "/views");
@@ -29,37 +31,36 @@ app.get("/", (req, res) => {
   res.send("Node server is running");
 });
 
-app.get("/preview", (req, res) => {
-  res.render("invite", { firstname: "John" });
-});
-
-app.post("/", (req, res) => {
+app.post("/send/template/invite", (req, res) => {
   var sender = req.body.sender;
+  var to = req.body.to;
   var recipient = req.body.recipient;
   var url = req.body.url;
 
   const options = {
-    from: "Babdev nodejs email sender",
-    to: "adeniyi.germany@gmail.com",
+    from: "teamCONNECT",
+    to,
     subject: "Invitation to chat on teamCONNECT",
     html: inviteTemplate({ recipient, sender, url }),
   };
 
   transporter.sendMail(options, (err, info) => {
+    var response = {};
+
     if (err) {
-      response = "There was an error sending the email";
+      response.status = "There was an error sending the email";
+      response.status_code = 500;
+
       console.log(err);
-      res.send(response);
+      res.send(JSON.stringify(response));
     } else {
-      response = "Email sent successfully";
+      response.status = "Email sent successfully";
+      response.status_code = 200;
+
       console.log(info.response);
-      res.send(response);
+      res.send(JSON.stringify(response));
     }
   });
 });
 
-server.listen(PORT, () => {
-  console.log("====================================");
-  console.log(`Server is running on port:  http://localhost:${PORT}`);
-  console.log("====================================");
-});
+server.listen(PORT);
